d189099c6a4d8cf2128daff2375aafb3a47fdec8
Diganth P <jayanth.jayanth.prakash@gmail.com>
2022-09-21 20:04:33 -0500

    filesupload

---
+++ application.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+application = app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", debug=True)
